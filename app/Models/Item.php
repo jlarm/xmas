5c78851460af2c59342f63686bc5411b02e972a7
jlarm <117611704+jlarm@users.noreply.github.com>
2024-10-15 22:10:20 -0500

    Item model and money package installed

---
+++ app/Models/Item.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Item extends Model
+{
+    protected $fillable = [
+        'name',
+        'size',
+        'color',
+        'link',
+        'price',
+        'purchased',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'purchased' => 'boolean',
+        ];
+    }
+}
