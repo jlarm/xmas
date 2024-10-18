2f788ab3363a0fb358a818a341b5a5571c3660ef
jlarm <117611704+jlarm@users.noreply.github.com>
2024-10-18 13:27:27 -0500

    Added key to kid loop

--- resources/views/livewire/kid-index.blade.php
+++ resources/views/livewire/kid-index.blade.php
@@ -1,12 +1,12 @@
 <flux:tab.group>
     <flux:tabs>
         @foreach ($kids as $kid)
-            <flux:tab :name="$kid->name">{{ $kid->name }}</flux:tab>
+            <flux:tab :name="$kid->name" wire:key="{{ $kid->id }}">{{ $kid->name }}</flux:tab>
         @endforeach
     </flux:tabs>
 
     @foreach ($kids as $kid)
-        <flux:tab.panel :name="$kid->name">
+        <flux:tab.panel :name="$kid->name" wire:key="{{ $kid->id }}">
             <div class="grid gap-10 md:grid-cols-3">
                 <div class="md:col-span-2">
                     <livewire:item-index :$kid />
