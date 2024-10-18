835e113be0f13b051d686d2ffdd5d078eebdafe0
jlarm <117611704+jlarm@users.noreply.github.com>
2024-10-18 12:58:38 -0500

    Added link and delete

--- app/Livewire/ItemIndex.php
+++ app/Livewire/ItemIndex.php
@@ -2,7 +2,9 @@
 
 namespace App\Livewire;
 
+use App\Models\Item;
 use App\Models\Kid;
+use Flux\Flux;
 use Livewire\Attributes\Computed;
 use Livewire\Attributes\On;
 use Livewire\Component;
@@ -19,6 +21,19 @@ class ItemIndex extends Component
     public function items()
     {
         return $this->kid->items()->latest()->paginate(8);
+    }
+
+    public function deleteItem($id): void
+    {
+        $item = Item::findOrFail($id);
+        $item->delete();
+        $this->dispatch('update-items');
+
+        Flux::toast(
+            heading: 'Deleted',
+            text: 'Item deleted successfully',
+            variant: 'success',
+        );
     }
 
     public function render()
--- resources/views/livewire/item-index.blade.php
+++ resources/views/livewire/item-index.blade.php
@@ -5,7 +5,7 @@
                 class="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400"
             >
                 <div class="min-w-0 flex-1">
-                    <a @if ($item->link) href="{{ $item->link }}" @endif target="_blank" class="focus:outline-none">
+                    <div class="focus:outline-none">
                         <span class="absolute inset-0" aria-hidden="true"></span>
                         <p class="text-sm font-medium text-gray-900">{{ $item->name }}</p>
                         <div class="mt-1">
@@ -23,25 +23,33 @@
                                 </flux:badge>
                             @endif
                         </div>
-                        @if ($item->link)
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                stroke-width="2"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                class="absolute right-3 top-3 size-4 text-gray-400"
-                            >
-                                <path d="M21 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h6" />
-                                <path d="m21 3-9 9" />
-                                <path d="M15 3h6v6" />
-                            </svg>
-                        @endif
-                    </a>
+                        <div class="absolute right-3 top-3">
+                            <flux:dropdown>
+                                <flux:button variant="subtle" icon="ellipsis-vertical"></flux:button>
+
+                                <flux:navmenu>
+                                    @if ($item->link)
+                                        <flux:navmenu.item
+                                            href="{{ $item->link }}"
+                                            target="_blank"
+                                            icon="arrow-top-right-on-square"
+                                        >
+                                            View
+                                        </flux:navmenu.item>
+                                    @endif
+
+                                    <flux:navmenu.item
+                                        wire:click="deleteItem({{ $item->id }})"
+                                        wire:confirm="Are you sure you want to delete this item?"
+                                        variant="danger"
+                                        icon="trash"
+                                    >
+                                        Delete
+                                    </flux:navmenu.item>
+                                </flux:navmenu>
+                            </flux:dropdown>
+                        </div>
+                    </div>
                 </div>
             </div>
         @endforeach
