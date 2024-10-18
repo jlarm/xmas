c9d23c4e660a40ceeb7f341170827cf257ccf6e0
jlarm <117611704+jlarm@users.noreply.github.com>
2024-10-18 13:36:23 -0500

    Manually including livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
         <link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
+        @livewireStyles
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @fluxStyles
     </head>
@@ -22,6 +23,7 @@
                 {{ $slot }}
             </main>
         </div>
+        @livewireScriptConfig
         @fluxScripts
         <flux:toast />
     </body>
