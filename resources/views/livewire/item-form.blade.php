2c1bf1ac696793777ad0cfd95213f39f540a7546
jlarm <117611704+jlarm@users.noreply.github.com>
2024-10-20 08:20:13 -0500

    Removed passcode modal

--- resources/views/livewire/item-form.blade.php
+++ resources/views/livewire/item-form.blade.php
@@ -4,8 +4,4 @@
         <flux:subheading>Add an item to your list.</flux:subheading>
     </div>
     <livewire:create-item :$kid hidePurchased="true" />
-    <flux:modal wire:model.self="showAuthModal" name="edit-profile" class="space-y-3 md:w-96">
-        <flux:input wire:model="passcode" label="Passcode" />
-        <flux:button wire:click="checkPasscode" type="submit" variant="danger" class="w-full">Submit</flux:button>
-    </flux:modal>
 </div>
